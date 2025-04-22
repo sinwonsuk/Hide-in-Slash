fa1c1c5e0bd00e6fe081ac3468c2f53259c632c2
sinwonsuk <onerpg3023@gmail.com>
2025-04-22 10:54:18 +0900

    업데이트

---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector2 vector = new Vector3(5.32f, 198.29f, 198.29f);
+
+        transform.position = vector;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
