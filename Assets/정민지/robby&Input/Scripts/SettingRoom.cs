b4c369c53be414ed03d80610a5aee2b69574b8ae
minji25 <zakijeong917@naver.com>
2025-04-22 23:06:29 +0900

    로비 세부사항 수정

--- Assets/정민지/robby&Input/Scripts/SettingRoom.cs
+++ Assets/정민지/robby&Input/Scripts/SettingRoom.cs
@@ -3,46 +3,99 @@ using UnityEngine;
 
 public class SettingRoom : MonoBehaviour
 {
-    public TMP_InputField roomNameInput;
-    public TMP_InputField passwordInput;
-    public Transform roomListContent;
-    public GameObject roomPanelPrefab; //룸
-    public GameObject popupPanel; // 팝업 루트 오브젝트
+    public TMP_InputField roomNameInput; //방이름 입력창
+    public TMP_InputField passwordInput; //방비번 입력창
+    public Transform roomListContent; //스크롤 콘텐트
+    public GameObject roomPanelPrefab; //생성할 방
+    public GameObject popupPanel; // 방 생성창
+
+    [Header("경고창")]
+    [SerializeField] private GameObject nicknameIsNull;
+    [SerializeField] private GameObject lengthExceeded;
+
+    private int warning = 0;
+
+    private GameObject currentWarning;  // 경고창 인스턴스를 저장할 변수
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            AddRoom();
+
+        if (Input.GetKeyDown(KeyCode.Escape) && warning == 0)
+        {
+            Time.timeScale = 1;
+            popupPanel.SetActive(false); // 팝업 닫기
+        }
+
+
+        if ((Input.GetKeyUp(KeyCode.Escape)) && (warning == 1 || warning == 2))
+        {
+            if (currentWarning != null)
+            {
+                Destroy(currentWarning);
+                Time.timeScale = 0;
+            }
+
+            warning = 0;
+        }
+    }
+
 
     public void OnClickConfirm()
+    {
+        AddRoom();
+    }
+
+    public void OnClickCancel()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    public void AddRoom()
     {
         string roomName = roomNameInput.text;
         string password = passwordInput.text;
 
         if (!string.IsNullOrEmpty(roomName) && !string.IsNullOrEmpty(password))
         {
-            GameObject room = Instantiate(roomPanelPrefab, roomListContent);
-
-
-            Transform roomNameObj = room.transform.Find("RoomNameText");
-
-            if (roomNameObj != null)
+            if (roomName.Length < 11 && password.Length < 6)
             {
-                roomNameObj.GetComponent<TMP_Text>().text = roomName; //방이름
+                GameObject room = Instantiate(roomPanelPrefab, roomListContent);
+
+
+                Transform roomNameObj = room.transform.Find("RoomNameText");
+
+                if (roomNameObj != null)
+                {
+                    roomNameObj.GetComponent<TMP_Text>().text = roomName; //방이름
+                }
+                else
+                {
+                    Debug.LogError("RoomNameText 또는 PasswordText 오브젝트를 프리팹에서 찾을 수 없습니다.");
+                }
+
+                popupPanel.SetActive(false); // 팝업 닫기
+                roomNameInput.text = "";
+                passwordInput.text = "";
             }
             else
             {
-                Debug.LogError("RoomNameText 또는 PasswordText 오브젝트를 프리팹에서 찾을 수 없습니다.");
+                if (currentWarning == null)
+                {
+                    currentWarning = Instantiate(lengthExceeded); // 현재 경고 = 길이 초과
+                }
+                warning = 2;
             }
 
-            popupPanel.SetActive(false); // 팝업 닫기
-            roomNameInput.text = "";
-            passwordInput.text = "";
         }
         else
         {
-            Debug.LogWarning("방 이름과 비밀번호를 입력해주세요.");
+            warning = 1;
+            if (currentWarning == null)
+                currentWarning = Instantiate(nicknameIsNull); // 현재 경고 = 텍스트 빔
         }
-    }
-
-    public void OnClickCancel()
-    {
-        popupPanel.SetActive(false);
     }
 
 }
