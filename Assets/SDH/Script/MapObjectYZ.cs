a29956f34b961abd525b7cf49621fd287e459931
donghwiSon <ehdgnlgk@gmail.com>
2025-04-24 01:13:36 +0900

    맵작업

--- Assets/SDH/Script/MapObjectYZ.cs
+++ Assets/SDH/Script/MapObjectYZ.cs
@@ -5,6 +5,5 @@ public class MapObjectYZ : MonoBehaviour
     private void Start()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
-        Debug.Log(transform.position.y + "  " + transform.position.z);
     }
 }
--- Assets/김보건/Scripts/map/MapManager.cs
+++ Assets/김보건/Scripts/map/MapManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public List<GameObject> mapObjects;
+    public GameObject miniGamePrefab;
 
     private Dictionary<string, GameObject> mapDic = new();
+    private List<Transform> eventSpawnPoints = new();
+    private List<Transform> playerSpawnPoints = new();
+    private List<int> espIndexs = new();
+    private List<int> pspIndexs = new();
     private string currentMap;
 
     void Start()
@@ -15,6 +20,10 @@ public class MapManager : MonoBehaviour
             Instantiate(mapObjects[i]);
             mapDic.Add(mapObjects[i].name, mapObjects[i]);
         }
+
+        AddSpawnPoints();
+        espIndexs = MakeRandomValues(10, eventSpawnPoints.Count);
+        pspIndexs = MakeRandomValues(5, playerSpawnPoints.Count);
     }
 
     public GameObject GetCurrentMap()
@@ -30,6 +39,35 @@ public class MapManager : MonoBehaviour
 
         currentMap = mapName;
         return mapDic[mapName];
+    }
+
+    private void AddSpawnPoints()
+    {
+        for (int i = 0; i < mapObjects.Count; i++)
+        {
+            foreach (Transform child in mapObjects[i].transform)
+            {
+                if (child.CompareTag("EventSpawnPoint"))
+                {
+                    eventSpawnPoints.Add(child);
+                }
+                else if (child.CompareTag("PlayerSpawnPoint"))
+                {
+                    playerSpawnPoints.Add(child);
+                }
+            }
+        }
+    }
+
+    private List<int> MakeRandomValues(int count, int maxValue)
+    {
+        HashSet<int> randomValues = new HashSet<int>();
+        while (randomValues.Count < count)
+        {
+            int randomValue = Random.Range(0, maxValue);
+            randomValues.Add(randomValue);
+        }
+        return new List<int>(randomValues);
     }
 
 }
