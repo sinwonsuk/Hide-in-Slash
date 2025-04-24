4f8e48e9c903c5c820472f62a89aca1eeb21eafe
donghwiSon <ehdgnlgk@gmail.com>
2025-04-24 15:23:12 +0900

    맵매니저

--- Assets/김보건/Scripts/map/MapManager.cs
+++ Assets/김보건/Scripts/map/MapManager.cs
@@ -1,10 +1,13 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager : MonoBehaviourPunCallbacks
 {
     public List<GameObject> mapObjects;
     public GameObject miniGamePrefab;
+    public GameObject playerPrefab;
 
     private Dictionary<string, GameObject> mapDic = new();
     private List<Transform> eventSpawnPoints = new();
@@ -15,16 +18,20 @@ public class MapManager : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < mapObjects.Count; i++)
+        if (PhotonNetwork.IsMasterClient)
         {
-            Instantiate(mapObjects[i]);
-            mapDic.Add(mapObjects[i].name, mapObjects[i]);
+            InitializeMap();
+            AddSpawnPoints();
+            espIndexs = MakeRandomValues(10, eventSpawnPoints.Count);
+            pspIndexs = MakeRandomValues(5, playerSpawnPoints.Count);
+            GenerateMiniGames();
         }
+        
 
-        AddSpawnPoints();
-        espIndexs = MakeRandomValues(10, eventSpawnPoints.Count);
-        pspIndexs = MakeRandomValues(5, playerSpawnPoints.Count);
+        
     }
+
+    
 
     public GameObject GetCurrentMap()
     {
@@ -41,6 +48,15 @@ public class MapManager : MonoBehaviour
         return mapDic[mapName];
     }
 
+
+    private void InitializeMap()
+    {
+        for (int i = 0; i < mapObjects.Count; i++)
+        {
+            Instantiate(mapObjects[i]);
+            mapDic.Add(mapObjects[i].name, mapObjects[i]);
+        }
+    }
     private void AddSpawnPoints()
     {
         for (int i = 0; i < mapObjects.Count; i++)
@@ -68,6 +84,26 @@ public class MapManager : MonoBehaviour
             randomValues.Add(randomValue);
         }
         return new List<int>(randomValues);
+    }
+
+    private void GenerateMiniGames()
+    {
+        for(int i = 0; i < espIndexs.Count; i++)
+        {
+            int index = espIndexs[i];
+            Transform spawnPoint = eventSpawnPoints[index];
+            GameObject miniGame = Instantiate(miniGamePrefab, spawnPoint.position, Quaternion.identity);
+        }
+    }
+
+    private void PlayerSpawn()
+    {
+        for (int i = 0; i < pspIndexs.Count; i++)
+        {
+            int index = pspIndexs[i];
+            Transform spawnPoint = playerSpawnPoints[index];
+            GameObject player = Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
+        }
     }
 
 }
