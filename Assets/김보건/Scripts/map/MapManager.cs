a18212cb616f50210f891e8ae75a05654ee4eca7
donghwiSon <ehdgnlgk@gmail.com>
2025-04-24 23:40:54 +0900

    ㅋㅁ

--- Assets/김보건/Scripts/map/MapManager.cs
+++ Assets/김보건/Scripts/map/MapManager.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class MapManager : MonoBehaviourPunCallbacks
 {
     public List<GameObject> mapObjects;
     public GameObject miniGamePrefab;
+    public GameObject generatorPrefab;
     public GameObject playerPrefab;
 
     private Dictionary<string, GameObject> mapDic = new();
@@ -14,9 +16,35 @@ public class MapManager : MonoBehaviourPunCallbacks
     private List<Transform> playerSpawnPoints = new();
     private List<int> espIndexs = new();
     private List<int> pspIndexs = new();
+    private string[] pRoles = { "Player1", "Player2", "Player3", "Player4", "Player5", "Player6", "Player7" };
+    private string[] monRoles = { "Mon1", "Mon2", "Mon3", "Mon4", "Mon5" };
     private string currentMap;
+    private int minigameCount = 10; //미니게임 개수
+    private int generatorCount = 5; //발전기 개수
+
+
+
+
 
     void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+
+
+
+
+
+
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public override void OnJoinedRoom()
     {
         if (PhotonNetwork.IsMasterClient)
         {
@@ -24,14 +52,11 @@ public class MapManager : MonoBehaviourPunCallbacks
             AddSpawnPoints();
             espIndexs = MakeRandomValues(10, eventSpawnPoints.Count);
             pspIndexs = MakeRandomValues(5, playerSpawnPoints.Count);
-            GenerateMiniGames();
-        }
-        
+            InitializeGenerators();
 
-        
+        }
     }
 
-    
 
     public GameObject GetCurrentMap()
     {
@@ -48,12 +73,17 @@ public class MapManager : MonoBehaviourPunCallbacks
         return mapDic[mapName];
     }
 
+    private void AssignRole()
+    {
+        Photon.Realtime.Player[] players = PhotonNetwork.PlayerList;
 
+    }
+    
     private void InitializeMap()
     {
         for (int i = 0; i < mapObjects.Count; i++)
         {
-            Instantiate(mapObjects[i]);
+            PhotonNetwork.Instantiate(mapObjects[i].name, mapObjects[i].transform.position, Quaternion.identity);
             mapDic.Add(mapObjects[i].name, mapObjects[i]);
         }
     }
@@ -86,25 +116,31 @@ public class MapManager : MonoBehaviourPunCallbacks
         return new List<int>(randomValues);
     }
 
-    private void GenerateMiniGames()
+    private void InitializeMinigames() //이벤트스폰포인트를 앞에서부터 미니게임을 개수만큼 스폰
     {
-        for(int i = 0; i < espIndexs.Count; i++)
+        for (int i = 0; i < minigameCount; i++)
         {
             int index = espIndexs[i];
             Transform spawnPoint = eventSpawnPoints[index];
-            GameObject miniGame = Instantiate(miniGamePrefab, spawnPoint.position, Quaternion.identity);
+            PhotonNetwork.Instantiate("MiniGame", spawnPoint.position, Quaternion.identity);
         }
     }
 
-    private void PlayerSpawn()
+    private void InitializeGenerators() //이벤트스폰포인트를 뒤에서부터 발전기를 개수만큼 스폰
     {
-        for (int i = 0; i < pspIndexs.Count; i++)
+        for (int i = 0; i < generatorCount; i++)
         {
-            int index = pspIndexs[i];
-            Transform spawnPoint = playerSpawnPoints[index];
-            GameObject player = Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
+            int index = espIndexs[espIndexs.Count - i - 1];
+            Transform spawnPoint = eventSpawnPoints[index];
+            PhotonNetwork.Instantiate("Generator", spawnPoint.position, Quaternion.identity);
         }
     }
+
+    private void InitializePlayers()
+    {
+        
+    }
+
 
 }
 
--- Assets/김보건/Scripts/map/PrisonDoor.cs
+++ Assets/김보건/Scripts/map/PrisonDoor.cs
@@ -27,15 +27,13 @@ public class PrisonDoor : MonoBehaviour
     private void OpenDoor()
     {
         Debug.Log("°¨¿Á ¹® ¿­¸²");
-        //anim.SetTrigger("Open");  ¾Ö´Ï¸ÞÀÌ¼Ç
         prisonDoor.SetActive(false);
         StartCoroutine(CloseDoorDelay(5f));
     }
 
     private void CloseDoor()
     {
-        Debug.Log("°¨¿Á ¹® ´ÝÈû");
-        //anim.SetTrigger("Close");   
+        Debug.Log("°¨¿Á ¹® ´ÝÈû");   
         prisonDoor.SetActive(true);
     }
 
