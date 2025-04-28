b77d91283c3286045393e10ed70b093448376856
minji25 <zakijeong917@naver.com>
2025-04-29 01:25:08 +0900

    처형씬 이후 플레이어 게임오버씬 프리팹 추가

---
+++ Assets/정민지/playerDeath/playerDeath.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerDeath : MonoBehaviour
+{
+    [SerializeField] private float fadeDuration = 2f; // 서서히 투명해지는 시간(초)
+    [SerializeField] private Image imageToFade;   // 투명하게 만들 이미지
+
+    private void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut()
+    {
+        Color originalColor = imageToFade.color;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            imageToFade.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+
+        // 완전히 투명하게 만든 뒤 alpha를 0으로 고정
+        imageToFade.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+        imageToFade.gameObject.SetActive(false);
+    }
+}
