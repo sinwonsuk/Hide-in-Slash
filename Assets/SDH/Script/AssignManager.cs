c807eef234030a50aa205263c71f9db1dcfd0c37
sinwonsuk <onerpg3023@gmail.com>
2025-04-29 23:10:07 +0900

    커밋

--- Assets/SDH/Script/AssignManager.cs
+++ Assets/SDH/Script/AssignManager.cs
@@ -65,7 +65,8 @@ public class AssignManager : MonoBehaviourPunCallbacks
     }
 
     public void asbvasdf()
-    { 
+    {
+        gameObject.SetActive(true);
 
         //게임으로 넘어가면
         //게임내부임
@@ -75,12 +76,15 @@ public class AssignManager : MonoBehaviourPunCallbacks
             InitializeMap(); // 맵 만들기
         }
         //맵 만들고 생성된 맵에 대한 정보는 각 클라이언트에서 저장
-        
-        
-        //다시 마스터만 진행
-        
 
-        StartCoroutine(Wait1()); // 대기시간 넣기
+
+        //다시 마스터만 진행
+
+
+
+        Wait1();
+
+        //StartCoroutine(Wait1()); // 대기시간 넣기
 
         //위에 할당 후 아래에서 서버 정보 활용하려면 좀 기다려줘야함
         //
@@ -88,13 +92,12 @@ public class AssignManager : MonoBehaviourPunCallbacks
         //
         //밑에 서버 정보 활용하는거 넣어야함
 
-         // 각 클라이언트에서 플레이어 생성
+        // 각 클라이언트에서 플레이어 생성
     }
 
-    IEnumerator Wait1()
+    void Wait1()
     {
-        yield return new WaitForSeconds(0.5f);
-
+      
         Debug.Log("대기끝");
         WriteDic();
         AddSpawnPoints();
@@ -108,8 +111,6 @@ public class AssignManager : MonoBehaviourPunCallbacks
             AssignSpawnPoint(); // 각 플레이어 스폰포인트 할당
             Debug.Log("방에서 마스터할일 완");
         }
-
-        yield return new WaitForSeconds(0.5f);
 
         Debug.Log("대기끝");
         InitializePlayers();
