559f4fd7f2aaa4c170679b39e1a50be8948ad031
sinwonsuk <onerpg3023@gmail.com>
2025-05-01 14:23:27 +0900

    커밋

--- Assets/신원석/Scripts/MemoryMinigameLineMove.cs
+++ Assets/신원석/Scripts/MemoryMinigameLineMove.cs
@@ -8,6 +8,8 @@ public class MemoryMinigameLineMove : MonoBehaviour, IBeginDragHandler, IDragHan
     void Start()
     {
         LineMiniGame = GetComponentInParent<LineMiniGame>();
+
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
    
@@ -21,18 +23,35 @@ public class MemoryMinigameLineMove : MonoBehaviour, IBeginDragHandler, IDragHan
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         transform.position = worldPos;
+        boxCollider2D.enabled = false;
+    }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject != gameObject && tag == collision.tag)
+        {
+            LineMiniGame.action.Invoke(check);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Collider2D hit = Physics2D.OverlapPoint(transform.position);
-        if (hit != null && this.CompareTag(hit.tag))
-        {
-            LineMiniGame.action.Invoke(check);
-        }     
-    }
 
+        boxCollider2D.enabled = true;
+        //Collider2D hit = Physics2D.OverlapPoint(transform.position);
+
+
+        //if (hit != null && hit.gameObject != gameObject && tag == hit.tag)
+        //{
+        //    LineMiniGame.action.Invoke(check);
+        //}
+        //else
+        //{
+        //    Debug.Log($"불일치 또는 자기 자신: this: {tag}, hit: {hit?.tag}");
+        //}
+
+    }
+    BoxCollider2D boxCollider2D;
     LineMiniGame LineMiniGame;
 
     [SerializeField]
