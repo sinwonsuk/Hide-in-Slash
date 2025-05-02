26f3fd0673f3658d28fc8c98a77aafe9be6fca18
minji25 <zakijeong917@naver.com>
2025-05-02 11:43:03 +0900

    비번 없어도 들어가지게 수정

--- Assets/정민지/robby&Input/Scripts/roomClick.cs
+++ Assets/정민지/robby&Input/Scripts/roomClick.cs
@@ -1,5 +1,9 @@
+using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 public class roomClick : MonoBehaviour
 {
@@ -7,18 +11,43 @@ public class roomClick : MonoBehaviour
 
     private string selectedRoomName;
 
+    // 방 목록 저장
+    public static List<RoomInfo> cachedRoomList = new List<RoomInfo>();
+
+
     public void OnRoomButtonClicked(string roomName)
     {
         selectedRoomName = roomName;
 
-        // 인스턴스 생성
-        GameObject panelInstance = Instantiate(passwordPanelPrefab);
+        // 캐시된 방 목록에서 찾기
+        RoomInfo roomInfo = cachedRoomList.FirstOrDefault(r => r.Name == selectedRoomName);
 
-        // 선택적으로, 패널 스크립트에 방 이름 전달
-        var enterRoom = panelInstance.GetComponent<EnterRoom>();
-        if (enterRoom != null)
+        // 비밀번호가 설정되어 있으면 패널을 띄우고, 없으면 바로 입장
+        if (roomInfo != null && roomInfo.CustomProperties.ContainsKey("pw"))
         {
-            enterRoom.SetRoomName(selectedRoomName);
+            string roomPassword = (string)roomInfo.CustomProperties["pw"];
+            if (!string.IsNullOrEmpty(roomPassword))
+            {
+                // 비밀번호 입력 패널 인스턴스 생성
+                GameObject panelInstance = Instantiate(passwordPanelPrefab);
+
+                // 선택적으로, 패널 스크립트에 방 이름 전달
+                var enterRoom = panelInstance.GetComponent<EnterRoom>();
+                if (enterRoom != null)
+                {
+                    enterRoom.SetRoomName(selectedRoomName);
+                }
+            }
+            else
+            {
+                // 비밀번호가 없으면 바로 입장
+                PhotonNetwork.JoinRoom(selectedRoomName);
+            }
+        }
+        else
+        {
+            // 비밀번호가 없으면 바로 입장
+            PhotonNetwork.JoinRoom(selectedRoomName);
         }
     }
 }
