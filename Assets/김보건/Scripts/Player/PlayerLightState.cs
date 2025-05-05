e8dbba913087b59f99963467221e463296017d54
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-05-05 21:12:27 +0900

    플레이어 코드정리

---
+++ Assets/김보건/Scripts/Player/PlayerLightState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerLightState : PlayerState
+{
+    public PlayerLightState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
