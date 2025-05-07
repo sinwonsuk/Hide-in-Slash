a67314c935e854f7b2e74b97e6763b5ef2f0b951
kang-dani <daeun4001@gmail.com>
2025-05-07 13:50:20 +0900

    커밋

--- Assets/SDH/Script/GameTimer.cs
+++ Assets/SDH/Script/GameTimer.cs
@@ -30,7 +30,7 @@ public class GameTimer : MonoBehaviourPunCallbacks
 
     IEnumerator enumerator()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
 
         if (PhotonNetwork.IsMasterClient)
         {
