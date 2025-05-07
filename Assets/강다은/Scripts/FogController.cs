b1013f3f59e0810b9626da89968211f88ca30464
sinwonsuk <onerpg3023@gmail.com>
2025-05-07 16:11:55 +0900

    안개 컨트롤러 오류 수정

--- Assets/강다은/Scripts/FogController.cs
+++ Assets/강다은/Scripts/FogController.cs
@@ -9,31 +9,59 @@ public class FogController : MonoBehaviour
     private float fogAmount = 0f;
     private float maxFogAmount = 1000f;
     private float fogIncreaseStep = 50f;
-
+    string bossName;
     private IEnumerator Start()
     {
         while(true)
         {
+
+
             if(AllPlayersHaveRoles())
             {
+                
 
-                string bossType = PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("Role", out object bossTypeObj)
-                    ? bossTypeObj as string
-                    : "";
-
-
-                if (bossType == "PukeGirlGhost")
+                if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("Role", out object selfRoleObj))
                 {
-                    gameObject.SetActive(true);
-                    fogEffect?.SetFloat("FogAmount", 20f);
-                    Debug.Log("Fog ON - 생존자 + PukeGirl 보스");
+                    if (selfRoleObj is string selfRole)
+                    {
+                        bossName = selfRole;
+                    }
                 }
-                else
+
+                // 만약 보스라면 
+
+                if (NetworkProperties.instance.GetMonsterStates(bossName) == true)
                 {
                     gameObject.SetActive(false);
-                    Debug.Log("Fog OFF - 보스가 PukeGirl 아님");
+                    yield break;
                 }
 
+                // 만약 플레이어 라면 
+
+                else if (NetworkProperties.instance.GetMonsterStates(bossName) == false)
+                {
+                    foreach (var player in PhotonNetwork.PlayerList)
+                    {
+                        if (player.CustomProperties.TryGetValue("Role", out object monster))
+                        {
+                            if (monster is string selfRole)
+                            {
+                                if (NetworkProperties.instance.GetMonsterStatesName(selfRole) == "PukeGirlGhost")
+                                {
+                                    gameObject.SetActive(true);
+                                    fogEffect?.SetFloat("FogAmount", 20.0f);
+                                    Debug.Log("Fog ON - 생존자 + 보스");
+                                    yield break;
+                                }
+                            }
+                        }
+                    }                    
+                }
+
+
+                gameObject.SetActive(false);
+                fogEffect?.SetFloat("FogAmount", 0.0f);
+                Debug.Log("Fog OFF - 생존자 + 보스");
                 yield break;
             }
             yield return null;
