7d8682dd8ce60a7c63ef72b16e9aa0dcc347d4b1
sinwonsuk <onerpg3023@gmail.com>
2025-05-09 15:06:19 +0900

    이벤트 매니저 오류 고침

--- Assets/신원석/Scripts/GeneratorManager.cs
+++ Assets/신원석/Scripts/GeneratorManager.cs
@@ -21,6 +21,12 @@ public class GeneratorManager : MonoBehaviourPunCallbacks
         }
     }
 
+    private void OnDestroy()
+    {
+        EventManager.UnRegisterEvent(EventType.GeneratorSuccess, addCount);
+    }
+
+
     [PunRPC]
     public void BroadcastSuecss()
     {
