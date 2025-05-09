9b88ef48bd3f3a571eea1932a8f304f27baa3d1d
kang-dani <daeun4001@gmail.com>
2025-05-09 14:03:30 +0900

    다은 - 판넬 수정

--- Assets/정민지/robby&Input/Scripts/GameReadyManager.cs
+++ Assets/정민지/robby&Input/Scripts/GameReadyManager.cs
@@ -590,6 +590,7 @@ public class GameReadyManager : MonoBehaviourPunCallbacks
     {
         // 방 나가기 시 로비로 돌아가기
         ClearSlots();
+        slotMap.Clear();
         profileOrder = null;
         nextProfilePointer = 0;
         isStartingGame = false;
@@ -748,6 +749,8 @@ public class GameReadyManager : MonoBehaviourPunCallbacks
                 return;
             }
 
+            RunnerPanel.SetActive(false);
+            RunnerBlack.SetActive(false);
             BossPanel.SetActive(true);
             BossBlack.SetActive(true);
             var bossR = BossPanel.GetComponentInChildren<playerDeath>();
@@ -768,6 +771,8 @@ public class GameReadyManager : MonoBehaviourPunCallbacks
                 return;
             }
 
+            BossPanel.SetActive(false);
+            BossBlack.SetActive(false);
             RunnerPanel.SetActive(true);
             RunnerBlack.SetActive(true);
             var playerR = RunnerPanel.GetComponentInChildren<playerDeath>(true); // (true) = 비활성화 오브젝트 포함
